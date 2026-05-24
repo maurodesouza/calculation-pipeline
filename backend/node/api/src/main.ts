1242499f55cda5366dda760cee0cb6a3ca18e02a
Mauro de Souza <maurodesouza2017@hotmail.com>
2026-05-24 18:06:34 -0300

    ⚡  chore: setup rabbitMQ adapter

--- backend/node/api/src/main.ts
+++ backend/node/api/src/main.ts
@@ -2,7 +2,7 @@ import { PGPromiseAdapter } from "./infra/database/pg-promise-adapter";
 import { Container } from "./infra/DI/container";
 import { HttpAdapter } from "./infra/http/http-adapter";
 import { PipelineRepositoryDAO } from "./infra/repository/pipeline";
-import { StepRepositoryDAO } from "./infra/repository/step";
+import { RabbitMQAdapter } from "./infra/queue/rabbitmq-adapter";
 import { CreatePipelineUseCase } from "./application/use-cases/create-pipeline";
 import { GetPipelineUseCase } from "./application/use-cases/get-pipeline";
 import { SyncStepsUseCase } from "./application/use-cases/sync-steps";
@@ -10,16 +10,22 @@ import { HTTPInterfaces } from "./interfaces/http";
 
 const PORT = 3000
 
-function api() {
+async function api() {
 	const httpServer = new HttpAdapter();
 	const pgPromiseAdapter = new PGPromiseAdapter();
+	const queue = new RabbitMQAdapter();
 
-	pgPromiseAdapter.connect();
+	await Promise.all([
+		pgPromiseAdapter.connect(),
+		queue.connect()
+	]);
+
 
 	const instance = Container.getInstance();
 
 	instance.register("http-server", httpServer);
 	instance.register("sql-connection", pgPromiseAdapter);
+	instance.register("queue", queue);
 	instance.register("pipeline-repository", new PipelineRepositoryDAO());
 	instance.register("step-repository", new StepRepositoryDAO());
 
