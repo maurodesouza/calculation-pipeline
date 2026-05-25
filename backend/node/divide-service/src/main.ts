b5babfb6162db56614bc15cd3cabde00fce9c3b3
Mauro de Souza <maurodesouza.me@gmail.com>
2026-05-25 01:03:10 -0300

    ✨ feat: add divide-service

---
+++ backend/node/divide-service/src/main.ts
@@ -0,0 +1,32 @@
+import { RabbitMQAdapter } from "./queue";
+
+type DividePayload = {
+	runId: string;
+	value: number;
+	stepId: string;
+	operation: string;
+	by: number;
+};
+
+async function main() {
+	const queue = new RabbitMQAdapter();
+	await queue.connect();
+
+	await Promise.all([
+		queue.setup("processor.events", "divide.execution.requested", { type: "direct", routingKey: "execution.division-requested" }),
+		queue.setup("divide.randomize", "randomizer", { type: "direct", routingKey: "execution.finished" }),
+		queue.setup("divide.events", "processor.execution.finished", { type: "direct", routingKey: "execution.finished" })
+	]);
+
+	queue.consume("divide.execution.requested", async (message: DividePayload) => {
+		const { runId, value, by } = message;
+
+		const result = value / by;
+
+		await queue.publish("divide.randomize", { runId, result }, { routingKey: "execution.finished" });
+	});
+
+	console.log("🚀 divide service is running...");
+}
+
+main()
