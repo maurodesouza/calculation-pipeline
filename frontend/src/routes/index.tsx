8816d9340427531176710144be700ed673443143
Mauro de Souza <maurodesouza.me@gmail.com>
2026-05-25 17:12:49 -0300

    ⚡ chore: add navigation link to pipelines page on home route

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Clickable } from "#/components/ui/clickable";
 
-export const Route = createFileRoute("/")({ component: App });
+export const Route = createFileRoute("/")({
+	component: App,
+});
 
 function App() {
 	return (
 		<main>
 			<h1>Calculation Pipeline</h1>
+
+			<Clickable.Link to="/pipelines">Pipelines</Clickable.Link>
 		</main>
 	);
 }
