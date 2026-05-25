abc24c764b843e295029e13790c3631fff1cf22b
Mauro de Souza <maurodesouza2017@hotmail.com>
2026-05-24 23:27:14 -0300

    ✨ feat: add dependency injection container with inject decorator

---
+++ backend/node/processor/src/infra/DI/container.ts
@@ -0,0 +1,35 @@
+export class Container {
+	private static instance: Container
+	private dependencies: Map<string, any> = new Map()
+
+	private constructor() {}
+
+	get(key: string) {
+		if (!this.dependencies.has(key)) {
+			throw new Error(`[container]: dependency ${key} not found`)
+		}
+
+		return this.dependencies.get(key)
+	}
+
+	register<T>(key: string, value: T): void {
+		this.dependencies.set(key, value)
+	}
+
+	static getInstance(): Container {
+		if (!Container.instance) {
+			Container.instance = new Container()
+		}
+		return Container.instance
+	}
+}
+
+export function inject(name: string) {
+	return function (target: any, propertyKey: string) {
+		target[propertyKey] = new Proxy({}, {
+			get: function (_, property) {
+				return Container.getInstance().get(name)[property]
+			}
+		})
+	}
+}
