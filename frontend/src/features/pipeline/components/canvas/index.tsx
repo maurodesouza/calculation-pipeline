532495326f6e46d2d2397ef2d8b647b2497c9455
Mauro de Souza <maurodesouza.me@gmail.com>
2026-05-27 02:38:06 -0300

    ✨ feat: add canvas component with drag-and-drop node creation and flow management

---
+++ frontend/src/features/pipeline/components/canvas/index.tsx
@@ -0,0 +1,74 @@
+import { useSelector } from "@tanstack/react-store";
+import type { Node, ReactFlowInstance } from "@xyflow/react";
+import { useCallback, useRef } from "react";
+import { v4 as uuid } from "uuid";
+import { Canvas as CanvasUI } from "#/components/ui/canvas";
+import { events } from "#/events";
+import { usePipelineContext } from "#/features/pipeline/store";
+import { nodeTypes } from "./nodes";
+
+export function Canvas() {
+	const { store } = usePipelineContext();
+	const flowInstance = useRef<ReactFlowInstance>(null);
+
+	const nodes = useSelector(store, (state) => state.nodes);
+	const edges = useSelector(store, (state) => state.edges);
+
+	const onDragOver = useCallback((event: React.DragEvent) => {
+		event.preventDefault();
+		event.dataTransfer.dropEffect = "move";
+	}, []);
+
+	const onDrop = useCallback((event: React.DragEvent) => {
+		event.preventDefault();
+
+		const data = event.dataTransfer?.getData("application/json");
+		if (!data) return;
+
+		const payload = JSON.parse(data);
+
+		const position = flowInstance.current?.screenToFlowPosition({
+			x: event.clientX,
+			y: event.clientY,
+		});
+
+		const newNode = {
+			id: uuid(),
+			type: "operation",
+			position,
+			data: { props: payload },
+			draggable: true,
+			selectable: true,
+			focusable: true,
+		} as Node;
+
+		events.pipelines.canvas.nodes.add(newNode);
+	}, []);
+
+	function onDragStart(event: React.DragEvent) {
+		event.dataTransfer.setData("text/plain", "operation");
+		event.dataTransfer.effectAllowed = "move";
+	}
+
+	return (
+		<CanvasUI.Container>
+			<CanvasUI.Flow
+				onInit={(instance) => {
+					flowInstance.current = instance;
+				}}
+				nodes={nodes}
+				edges={edges}
+				nodeTypes={nodeTypes}
+				onNodesChange={(event) => events.pipelines.canvas.nodes.change(event)}
+				onEdgesChange={(event) => events.pipelines.canvas.edges.change(event)}
+				onConnect={(event) => events.pipelines.canvas.edges.connect(event)}
+				onDrop={onDrop}
+				onDragStart={onDragStart}
+				onDragOver={onDragOver}
+				fitView
+			>
+				<CanvasUI.Background />
+			</CanvasUI.Flow>
+		</CanvasUI.Container>
+	);
+}
