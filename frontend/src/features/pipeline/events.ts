3e9b7f19cd0b98d05969cc90cf393633a28bddd7
Mauro de Souza <maurodesouza.me@gmail.com>
2026-05-27 02:36:56 -0300

    ✨ feat: add pipeline events enum and type definitions for canvas operations

---
+++ frontend/src/features/pipeline/events.ts
@@ -0,0 +1,31 @@
+import type { Connection, EdgeChange, Node, NodeChange } from "@xyflow/react";
+
+export enum PipelineEvents {
+	CANVAS_NODES_ADD = "pipelines.canvas.nodes.add",
+	CANVAS_NODES_REMOVE = "pipelines.canvas.nodes.remove",
+	CANVAS_NODES_CHANGE = "pipelines.canvas.nodes.change",
+	CANVAS_EDGES_CONNECT = "pipelines.canvas.edges.connect",
+	CANVAS_EDGES_CHANGE = "pipelines.canvas.edges.change",
+
+	SAVE_PIPELINE = "pipelines.save",
+}
+
+declare module "#/events/index" {
+	interface Events {
+		pipelines: {
+			save: () => void;
+
+			canvas: {
+				nodes: {
+					add: (payload: Node | Node[]) => void;
+					remove: (payload: string | string[]) => void;
+					change: (payload: NodeChange[]) => void;
+				};
+				edges: {
+					connect: (payload: Connection) => void;
+					change: (payload: EdgeChange[]) => void;
+				};
+			};
+		};
+	}
+}
