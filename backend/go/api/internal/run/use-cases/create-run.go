816f912eb8ea40c0eb5c25b12779e2cb5ab16dc7
Mauro de Souza <maurodesouza.me@gmail.com>
2026-06-12 21:41:33 -0300

    ✨ feat: add create run use-case

---
+++ backend/go/api/internal/run/use-cases/create-run.go
@@ -0,0 +1,55 @@
+package usecases
+
+import (
+	"context"
+
+	"github.com/maurodesouza/calculation-pipeline/backend/go/api/internal/pipeline"
+	"github.com/maurodesouza/calculation-pipeline/backend/go/api/internal/queue"
+	runPkg "github.com/maurodesouza/calculation-pipeline/backend/go/api/internal/run"
+)
+
+type CreateRunUseCaseDependencies struct {
+	Queue              queue.RabbitMQ
+	RunRepository      *runPkg.RunRepository
+	PipelineRepository *pipeline.PipelineRepository
+}
+
+type CreateRunUseCase struct {
+	CreateRunUseCaseDependencies
+}
+
+func NewCreateRunUseCase(deps CreateRunUseCaseDependencies) CreateRunUseCase {
+	return CreateRunUseCase{
+		CreateRunUseCaseDependencies: deps,
+	}
+}
+
+type CreateRunInput struct {
+	PipelineId *string
+	Payload    float64
+}
+
+func (u *CreateRunUseCase) Execute(input CreateRunInput) error {
+	pipeline, err := u.PipelineRepository.GetByID(context.Background(), *input.PipelineId)
+	if err != nil {
+		return err
+	}
+
+	run, err := runPkg.NewRun(runPkg.NewRunPayload{
+		PipelineId: input.PipelineId,
+		Payload:    input.Payload,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	err = u.RunRepository.Save(context.Background(), run)
+	if err != nil {
+		return err
+	}
+
+	u.Queue.Publish("run.created", runPkg.RunCreatedMapper(run, pipeline), queue.PublishConfig{})
+
+	return nil
+}
